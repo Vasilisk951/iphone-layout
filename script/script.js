6a3805b4b1d62891eb38f3f0e1835de51e7b9c93
Constantine Medvedev <vasilisk951@gmail.com>
2021-03-11 14:21:52 +0300

    customized the characteristics and appearances of the modal window

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
 
+    // выбор товара
     const tabs = () => {
         /* const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
         const cardDetailsTitleElems = document.querySelectorAll('.card-details__title');
@@ -31,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const cardImageItem = document.querySelector('.card__image_item');
         const cardDetailsPriceElem = document.querySelector('.card-details__price');
         const descriptionMemory = document.querySelector('.description__memory');
+        const characteristicsItemDescriptionMemory = document.querySelector('.characteristics__item-description-memory');
 
         const data = [
             {
@@ -70,15 +70,84 @@ document.addEventListener('DOMContentLoaded', () => {
                     cardImageItem.alt = data[i].name;
                     cardDetailsPriceElem.textContent = data[i].price + '₽';
                     descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memoryRom} ГБ`;
+                    characteristicsItemDescriptionMemory.textContent = data[i].memoryRom;
+
 
                 }
             })
         })
 
-
-
-
-
     }
+
+    // характеристики 
+    const accordion = () => {
+        const characteristicsListElem = document.querySelector('.characteristics__list');
+        const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+
+        // открываем характетистику
+        const open = (button, dropDown) => {
+            closeAllDrops();
+            dropDown.style.height = `${dropDown.scrollHeight}px`;
+            button.classList.add('active');
+            dropDown.classList.add('active');
+        }
+
+        // закрываем характеристики 
+        const close = (button, dropDown) => {
+            button.classList.remove('active');
+            dropDown.classList.remove('active');
+            dropDown.style.height = '';
+        }
+
+        // закрываем предыдущие открытиые пункты характеристик
+        const closeAllDrops = () => {
+            characteristicsItemElems.forEach((elem) => {
+                if(elem.children[0].classList.contains('active') && elem.children[1].classList.contains('active')) {
+                    close(elem.children[0], elem.children[1]);
+                }
+            })
+        }
+
+        // событие клика и запуск функций открытия и закрытия
+        characteristicsListElem.addEventListener('click', (event) => {
+            const target = event.target;
+            if(target.classList.contains('characteristics__title')) {
+                const parent = target.closest('.characteristics__item');
+                const description = parent.querySelector('.characteristics__description');
+                description.classList.contains('active') ? close(target, description) : open(target, description);
+            }
+        });
+
+
+    };
+
+    const modal = () => {
+        const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
+        const cardDetailsButtonDelivery = document.querySelector('.card-details__button_delivery');
+        const modal = document.querySelector('.modal');
+        const modalSubtitle = document.querySelector('.modal__subtitle');
+
+        cardDetailsButtonBuy.addEventListener('click', () => {
+            modal.classList.add('open');
+            modalSubtitle.innerHTML = `Оплата`;
+        })
+        cardDetailsButtonDelivery.addEventListener('click', () => {
+            modal.classList.add('open');
+            modalSubtitle.innerHTML = `Доставка и оплата`;
+        })
+        modal.addEventListener('click', (event) => {
+            const target = event.target;
+            if(target.classList.contains('modal__close') || target.classList.contains('modal')) {
+                modal.classList.remove('open');
+            }
+        })
+    }
+       
+
+
+
     tabs();
+    accordion();
+    modal();
+
 });
